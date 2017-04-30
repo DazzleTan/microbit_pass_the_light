a3bfb0cdcfba2cd0cf42d9c2c7c0ba97887247da
Matthew Biesen <upandcomming88@gmail.com>
2017-04-29 22:30:27 -0500

    Add files via upload

---
+++ marco_polo1.py
@@ -0,0 +1,32 @@
+import radio
+import random
+from microbit import display, Image, button_a, sleep
+
+# Create the "flash" animation frames. Can you work out how it's done?
+myflash = [Image().invert()*(i/9) for i in range(9, -1, -1)]
+
+# The radio won't work unless it's switched on.
+radio.on()
+
+# Event loop.
+while True:
+    # Button A sends a "flash" message.
+    if button_a.was_pressed():
+        radio.send('one')  # a-ha
+    # Read any incoming messages.
+    incoming = radio.receive()
+    if incoming == 'four' or incoming == 'fire':
+        incoming = 'nothing'
+        # If there's an incoming "flash" message display
+        # the firefly flash animation after a random short
+        # pause.
+        sleep(random.randint(50, 350))
+        display.show(myflash, delay=100, wait=False)
+        # Randomly re-broadcast the flash message after a
+        # slight delay.
+        sleep(1000)
+        radio.send('one')
+        #if random.randint(0, 9) == 0:
+            #sleep(500)
+            #radio.send('flash')  # a-ha
+    
---
+++ marco_polo3.py
@@ -0,0 +1,34 @@
+import radio
+import random
+from microbit import display, Image, button_a, button_b, sleep
+
+# Create the "flash" animation frames. Can you work out how it's done?
+myflash = [Image().invert()*(i/9) for i in range(9, -1, -1)]
+
+# The radio won't work unless it's switched on.
+radio.on()
+
+# Event loop.
+while True:
+    # Button A sends a "flash" message.
+    if button_a.was_pressed():
+        radio.send('three')  # a-ha
+    if button_b.was_pressed():
+        radio.send('fire')
+    # Read any incoming messages.
+    incoming = radio.receive()
+    if incoming == 'two' or incoming == 'fire':
+        incoming = 'nothing'
+        # If there's an incoming "flash" message display
+        # the firefly flash animation after a random short
+        # pause.
+        sleep(random.randint(50, 350))
+        display.show(myflash, delay=100, wait=False)
+        # Randomly re-broadcast the flash message after a
+        # slight delay.
+        sleep(3000)
+        radio.send('three')
+        #if random.randint(0, 9) == 0:
+            #sleep(500)
+            #radio.send('flash')  # a-ha
+    
