85e17572bb5c6229313a038d56eeedd60b2f500a
Matthew Biesen <upandcomming88@gmail.com>
2017-04-29 22:39:06 -0500

    Update pass_the_light.py

--- pass_the_light.py
+++ pass_the_light.py
@@ -2,6 +2,7 @@ import radio
 import random
 from microbit import display, Image, button_a, sleep
 
+# my_id is the id of this microbit.  Call id is the next microbit in the ordinal sequence
 my_id = 'four'
 call_id = 'one'
 everyone_called = 'fire'
@@ -14,24 +15,18 @@ radio.on()
 
 # Event loop.
 while True:
-    # Button A sends a "flash" message.
+    # Button A sends a call to the next microbit in sequence.  Used to kick off program
     if button_a.was_pressed():
-        radio.send(call_id)  # a-ha
+        radio.send(call_id)
+    # Button B sends a call out to all other microbits to light up.
     if button_b.was_pressed():
         radio.send(everyone_called)
     # Read any incoming messages.
     incoming = radio.receive()
     if incoming == my_id or incoming == everyone_called:
         incoming = 'nothing'
-        # If there's an incoming "flash" message display
-        # the firefly flash animation after a random short
-        # pause.
         sleep(random.randint(50, 350))
         display.show(myflash, delay=100, wait=False)
-        # Randomly re-broadcast the flash message after a
-        # slight delay.
         sleep(1000)
         radio.send(call_id)
-        #if random.randint(0, 9) == 0:
-            #sleep(500)
-            #radio.send('flash')  # a-ha
+
