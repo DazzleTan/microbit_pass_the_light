f056fd0487ac4b7b30fa27eff756489bb6b4294f
Matthew Biesen <upandcomming88@gmail.com>
2017-04-29 22:35:33 -0500

    Update marco_polo4.py

--- marco_polo4.py
+++ marco_polo4.py
@@ -1,6 +1,10 @@
 import radio
 import random
-from microbit import display, Image, button_a, button_b, sleep
+from microbit import display, Image, button_a, sleep
+
+my_id = 'four'
+call_id = 'one'
+everyone_called = 'fire'
 
 # Create the "flash" animation frames. Can you work out how it's done?
 myflash = [Image().invert()*(i/9) for i in range(9, -1, -1)]
@@ -12,12 +16,12 @@ radio.on()
 while True:
     # Button A sends a "flash" message.
     if button_a.was_pressed():
-        radio.send('four')  # a-ha
+        radio.send(call_id)  # a-ha
     if button_b.was_pressed():
-        radio.send('fire')
+        radio.send(everyone_called)
     # Read any incoming messages.
     incoming = radio.receive()
-    if incoming == 'three' or incoming == 'fire':
+    if incoming == my_id or incoming == everyone_called:
         incoming = 'nothing'
         # If there's an incoming "flash" message display
         # the firefly flash animation after a random short
@@ -26,9 +30,8 @@ while True:
         display.show(myflash, delay=100, wait=False)
         # Randomly re-broadcast the flash message after a
         # slight delay.
-        sleep(3000)
-        radio.send('four')
+        sleep(1000)
+        radio.send(call_id)
         #if random.randint(0, 9) == 0:
             #sleep(500)
             #radio.send('flash')  # a-ha
-    
